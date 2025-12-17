c7d63b357fe6b49443d2a01b55da6ae3452607cb
newlin76710 <newlin76710@gmail.com>
2025-12-18 01:36:03 +0800

    Update ChatApp.jsx

--- src/pages/ChatApp.jsx
+++ src/pages/ChatApp.jsx
@@ -241,7 +241,7 @@ export default function ChatApp() {
           </div>
         </div>
 
-        <div className="user-list">
+        {/* <div className="user-list">
           <strong>在線：{userList.length}</strong>
           {userList.map((u) => (
             <div
@@ -253,7 +253,7 @@ export default function ChatApp() {
               {u.name} (Lv.{u.level})
             </div>
           ))}
-        </div>
+        </div> */}
       </div>
 
       <SongPanel socket={socket} room={room} name={name} />
