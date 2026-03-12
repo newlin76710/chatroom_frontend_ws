0bedfc5982524d07dfb23aff07ed76abf09407e6
newlin76710 <newlin76710@gmail.com>
2026-03-12 09:35:01 +0800

    65

--- src/pages/aiConfig.js
+++ src/pages/aiConfig.js
@@ -22,6 +22,7 @@ export const aiAvatars = {
   "小龍女2": "/avatars/g10.gif",
   "神鍵墨客2": "/avatars/b10.gif",
   "非常天蠍": "/avatars/j64.gif",
+  "楓鈴": "/avatars/j65.gif",
 };
 
 export const aiProfiles = {
